5ff1b69696b36d394df895dd9830876f777b8c30
mullernicolli <niscolem@hotmail.com>
2026-01-22 23:56:50 -0300

    extras

--- cypress/e2e/gui/home.cy.js
+++ cypress/e2e/gui/home.cy.js
@@ -15,4 +15,25 @@ describe('Acessa a home', () => {
         cy.visit('/home')
         cy.wait('@getHome')
     })
+
+    it('UI retorna mesma quantidade da API de produtos nas vitrines', () => {  // comparação bruta sem aplicação de regras!
+        cy.intercept({
+            method: 'GET',
+            url: '**/home/tpPagina/HOME*segmento=Shopping*',
+        }).as('getProducts')
+
+        cy.visit('/home')
+        cy.wait('@getProducts')
+        .then((interception) => {
+            const itensLayout = interception.response.body.itensLayout
+            const vitrinesDaHome = itensLayout
+                .filter(item => item.vitrines)
+                .flatMap(item => item.vitrines)
+
+            const totalProdutosAPI = vitrinesDaHome.reduce((total, vitrine) => total + vitrine.itensVitrine.length, 0)
+
+            cy.get('.grid-card')
+                .should('have.length', totalProdutosAPI)
+        })
+    })
 })
